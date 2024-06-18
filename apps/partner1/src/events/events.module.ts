b2f2bf9260262c91943406fc27edb5e0d768b123
pedro henrique <pedro.henrike2812@gmail.com>
2024-06-18 20:11:29 +0000

    wip: config setup database with docker

--- apps/partner1/src/events/events.module.ts
+++ apps/partner1/src/events/events.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
-import { EventsService } from '@app/core/events/events.service';
 import { EventsCoreModule } from '@app/core/events/events-core.module';
 
 @Module({
--- libs/core/src/events/events-core.module.ts
+++ libs/core/src/events/events-core.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   providers: [EventsService],
-  exports: [EventsService]
+  exports: [EventsService, PrismaService],
 })
 export class EventsCoreModule {}
